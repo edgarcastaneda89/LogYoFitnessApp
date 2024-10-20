91dab38fe2fae678cbdbc6a7ffc55adefe4d91ae
edgarcastaneda89 <edgarcastaneda89@yahoo.com>
2024-10-20 14:31:19 -0700

    Updated Amplify

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import '@aws-amplify/ui-react/styles.css';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
+// Import Amplify and aws-exports configuration
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { ThemeProvider, Authenticator } from '@aws-amplify/ui-react'; // Import Authenticator
+
+// Configure Amplify
+Amplify.configure(awsconfig);
+
+// Wrap your app in ThemeProvider and Authenticator.Provider
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <Authenticator.Provider>  {/* Authenticator wraps the app */}
+        <App />
+      </Authenticator.Provider>
+    </ThemeProvider>
   </StrictMode>,
-)
+);
